4abfdf2d22c773edd0152d52f274c819b3cceb18
maviakazi12 <mavia.kazi@myob.com>
2025-03-25 09:59:48 +1100

    finalize code

--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -10,28 +10,24 @@ public class StringCalculator
         if (numbers == "") return 0;
         if (numbers.StartsWith("//"))
         {
-            if (numbers.Contains("[") && numbers.Contains("]"))
+            if (numbers.Contains('[') && numbers.Contains(']'))
             {
                 string pattern = @"\[(.*?)\]";
-                MatchCollection matches= Regex.Matches(numbers, pattern);
+                MatchCollection matches = Regex.Matches(numbers, pattern);
                 foreach(Match match in matches){
                     delimiter.Add(match.Groups[1].Value );
                 }
-                // start = numbers.IndexOf("[") + 1;
-                // end = numbers.IndexOf("]");
             }
             else
             {
                 start = numbers.IndexOf("//") + 2;
                 end = numbers.IndexOf('\n');
-            }
-
-            string singleDelimiter = numbers.Substring(start, end - start);
-            delimiter.Add(singleDelimiter);
+                string singleDelimiter = numbers.Substring(start, end - start);
+                delimiter.Add(singleDelimiter);
+            }            
             char[] charsToRemove = { '/', '\\', '\n', '[', ']' };
-            string newNumbers = new string(numbers.Where(n => !charsToRemove.Contains(n)).ToArray());
-            result = newNumbers.Split(delimiter.ToArray(), StringSplitOptions.RemoveEmptyEntries).ToList();
-            // numbers = newNumbers;
+            string numbersWithRemovedSymbols = new string(numbers.Where(n => !charsToRemove.Contains(n)).ToArray());
+            result = numbersWithRemovedSymbols.Split(delimiter.ToArray(), StringSplitOptions.RemoveEmptyEntries).ToList();
         }
         ;
         if (!result.Any())
@@ -71,8 +67,6 @@ public class StringCalculator
             throw new ArgumentException("Negatives not allowed: " + string.Join(", ", negativeNumbers));
         }
         return sum;
-        //    Console.WriteLine(string.Join(", ",result));
-
     }
 }
 
