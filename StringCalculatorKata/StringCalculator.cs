67a9536d43c8e82e8add56e7348c213a065096fb
maviakazi12 <mavia.kazi@myob.com>
2025-03-25 14:21:51 +1100

    Add more methods

--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -3,41 +3,53 @@ public class StringCalculator
 {
     public int Add(string numbers)
     {
-        List<string> delimiter = new List<string>();
         List<string> result = new List<string>();
-        int start = 0;
-        int end = 0;
         if (numbers == "") return 0;
         if (numbers.StartsWith("//"))
         {
-            if (numbers.Contains('[') && numbers.Contains(']'))
-            {
-                string pattern = @"\[(.*?)\]";
-                MatchCollection matches = Regex.Matches(numbers, pattern);
-                foreach(Match match in matches){
-                    delimiter.Add(match.Groups[1].Value );
-                }
-            }
-            else
-            {
-                start = numbers.IndexOf("//") + 2;
-                end = numbers.IndexOf('\n');
-                string singleDelimiter = numbers.Substring(start, end - start);
-                delimiter.Add(singleDelimiter);
-            }            
-            char[] charsToRemove = { '/', '\\', '\n', '[', ']' };
-            string numbersWithRemovedSymbols = new string(numbers.Where(n => !charsToRemove.Contains(n)).ToArray());
-            result = numbersWithRemovedSymbols.Split(delimiter.ToArray(), StringSplitOptions.RemoveEmptyEntries).ToList();
+            List<string> delimiter = ExtractDelimiter(numbers);
+            result = ExtractNumbers(numbers, delimiter);
         }
         ;
         if (!result.Any())
         {
             result = numbers
-        .Split(',', '\n', ';').ToList()
-        .Where(s => !string.IsNullOrWhiteSpace(s))
-        .ToList();
+                    .Split(',', '\n', ';').ToList()
+                    .Where(s => !string.IsNullOrWhiteSpace(s))
+                    .ToList();
         }
+        return CalculateSum(result);
+    }
+    private List<string> ExtractDelimiter(string numbers)
+    {
+        List<string> delimiter = new List<string>();
 
+        if (numbers.Contains('[') && numbers.Contains(']'))
+        {
+            string pattern = @"\[(.*?)\]";
+            MatchCollection matches = Regex.Matches(numbers, pattern);
+            foreach (Match match in matches)
+            {
+                delimiter.Add(match.Groups[1].Value);
+            }
+        }
+        else
+        {
+            int start = numbers.IndexOf("//") + 2;
+            int end = numbers.IndexOf('\n');
+            string singleDelimiter = numbers.Substring(start, end - start);
+            delimiter.Add(singleDelimiter);
+        }
+        return delimiter;
+    }
+    private List<string> ExtractNumbers(string numbers, List<string> delimiter)
+    {
+        char[] charsToRemove = { '/', '\\', '\n', '[', ']' };
+        string numbersWithRemovedSymbols = new string(numbers.Where(n => !charsToRemove.Contains(n)).ToArray());
+        return numbersWithRemovedSymbols.Split(delimiter.ToArray(), StringSplitOptions.RemoveEmptyEntries).ToList();
+    }
+    private int CalculateSum(List<string> result)
+    {
         int sum = 0;
         List<int> negativeNumbers = new List<int>();
         foreach (var num in result)
